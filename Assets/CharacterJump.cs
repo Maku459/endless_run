5d783b10ecf502d1622840f2f3bdc83d8aa24478
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-08-22 03:11:00 +0900

    character jump

--- Assets/CharacterJump.cs
+++ Assets/CharacterJump.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CharacterJump : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +13,15 @@ public class CharacterJump : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+        var target = GameObject.Find("Character");
+        if (Input.GetMouseButtonDown(0))
+        {
+            iTween.MoveBy(target, iTween.Hash("y", 1f, "time", 1f, "delay", 0f, "easetype", "easeOutCubic"));
+            iTween.MoveBy(target, iTween.Hash("y", -1f, "time", 0.8f, "delay", 1.0f, "easetype", "easeInCubic"));
+            iTween.ScaleTo (target, iTween.Hash ("y", 0.7, "time", 1f, "delay", 0f, "easetype", "easeOutCubic"));
+            iTween.ScaleTo (target, iTween.Hash ("y", 0.5, "time", 0.8f, "delay", 0.9f, "easetype", "easeInCubic"));
+            iTween.ScaleTo (target, iTween.Hash ("y", 0.617, "time", 0.2f, "delay", 1.8f, "easetype", "easeInQuart"));
+        }
     }
 }
