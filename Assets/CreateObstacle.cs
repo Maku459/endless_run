145f7ffb402b24e7a4bf9f0c3bd310e960cea8ed
Kyoko Sakuma <hzk.butterfly459@gmail.com>
2021-09-13 03:31:55 +0900

    correct position generate

--- Assets/CreateObstacle.cs
+++ Assets/CreateObstacle.cs
@@ -15,12 +15,13 @@ public class CreateObstacle : MonoBehaviour
         if(this.delta > this.span)
         {
             this.delta = 0;
-            //float px = Random.Range((float) -0.05, (float) 0.07);
             float[] pxArray = new float[3] {-0.08f, 0, 0.08f};
             var px = pxArray[Random.Range(0,3)];
-            GameObject go = Instantiate(arrowPrefab, new Vector3(px, (float)-0.202, (float) -0.524), Quaternion.identity) as GameObject;　//InstantiateメソッドはObject型を返すが、GameObject型で受け取りたいのでキャストという強制型変換をしている
-            //GameObject go = Instantiate (arrowPrefab, parent.transform, false);
-            go.transform.SetParent(parent.transform, false);
+            GameObject go = Instantiate(arrowPrefab, new Vector3(px,(float)-3.38,(float)-5.57), Quaternion.identity) as GameObject;　//InstantiateメソッドはObject型を返すが、GameObject型で受け取りたいのでキャストという強制型変換をしている
+            Vector3 size = go.transform.localScale; //現在の大きさを代入
+            size = size * 10;
+            go.transform.localScale = size;
+            go.transform.SetParent(parent.transform, true);
         }
     }
 }
